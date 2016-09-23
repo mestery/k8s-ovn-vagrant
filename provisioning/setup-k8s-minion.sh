987c87530dd36d753dbaf598a0435edbbe6284e3
Kyle Mestery <mestery@mestery.com>
2016-09-23 11:24:36 -0500

    We do not need to run etcd on minion nodes
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- provisioning/setup-k8s-minion.sh
+++ provisioning/setup-k8s-minion.sh
@@ -22,10 +22,6 @@ sudo service docker start
 
 # Install k8s
 
-# Install an etcd cluster
-sudo docker run --net=host -d gcr.io/google_containers/etcd:2.0.12 /usr/local/bin/etcd \
-                --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
-
 # Download Kubernetes ... yes, it's huge
 #mkdir k8s
 #pushd k8s
